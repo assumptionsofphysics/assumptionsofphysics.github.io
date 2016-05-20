dd110e47b791bb719e60fec92b94d2a019fd0e0f
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2016-05-20 10:41:09 -0400

    Adding script to build latest draft

---
+++ scripts/build-pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+rm -rf build
+mkdir build
+cd build
+git clone https://github.com/gcarcassi/physics.git
+cd physics/2015CompleteDerivation/
+pdflatex completeDerivation.tex
+cp completeDerivation.tex ../../../../draft.pdf
