9c258cc2301660483d3a7cfd845614f051ac2b65
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2016-07-17 17:08:46 -0400

    References not working

--- scripts/build-pdf.sh
+++ scripts/build-pdf.sh
@@ -6,6 +6,8 @@ mkdir build
 cd build
 git clone https://github.com/gcarcassi/physics.git
 cd physics/2015CompleteDerivation/
+latex completeDerivation.tex
+bibtex completeDerivation.tex
 pdflatex completeDerivation.tex
 cp completeDerivation.pdf ../../../../draft.pdf
 cd ../../../..
